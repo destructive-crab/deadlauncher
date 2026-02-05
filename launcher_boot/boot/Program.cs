45c78838c62b62313de9c3dfbdc59ad5f3842039
destructive_crab <destructivecrab@gmail.com>
2026-02-05 17:55:05 +0300

    progress window

--- launcher_boot/boot/Program.cs
+++ launcher_boot/boot/Program.cs
@@ -1,20 +1,31 @@
 ﻿using System.Diagnostics;
 using System.Net;
 using System.Runtime.InteropServices;
-using boot;
+using Raylib_cs;
 
 public class DeadLauncherBoot
 {
     private const string GithubUsername = "destructive-crab";
     private const string GithubRepositoryName = "deadlauncher";
     private const string GithubLauncherTag = "launcher";
-    private const string AssetName = "deadlauncher";
+    public static string AssetName()
+    {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            return "deadlauncher.exe";
+        }
+        else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+        {
+            return "deadlauncher";
+        }
+        return "invalid";
+    }
 
     public static readonly string LauncherFolderPath = Path.Combine("deadlauncher");
     public static readonly string DataFolderPath = Path.Combine(LauncherFolderPath, "data");
     
     public static readonly string ConfigPath = Path.Combine(DataFolderPath, "config.json");
-    public static readonly string LauncherExecutablePath = Path.Combine(LauncherFolderPath, "deadlauncher");
+    public static readonly string LauncherExecutablePath = Path.Combine(LauncherFolderPath, AssetName());
     public static readonly string LauncherVersionPath = Path.Combine(DataFolderPath, "launcher_version");
 
     public static string GetFullPath(string path)
@@ -27,12 +38,11 @@ public class DeadLauncherBoot
     {
         Booter booter = new Booter();
 
-        return;
         await booter.ValidateFolders();
         await booter.StartLauncher();
     }
 
-    class Booter
+    private class Booter
     {
         public async Task ValidateFolders()
         {
@@ -63,6 +73,7 @@ public class DeadLauncherBoot
             //update logic
             WebClient webClient = new();
             GitHubClient client = new GitHubClient(GithubUsername, GithubRepositoryName);
+            
             string id = webClient.DownloadString(new Uri(client.GetAssetDownloadURL(GithubLauncherTag, "version")));
 
             if (isLauncherInstalled)
@@ -94,10 +105,14 @@ public class DeadLauncherBoot
                 File.Delete(GetFullPath(LauncherExecutablePath));
             }
 
-            string downloadLink = client.GetAssetDownloadURL(GithubLauncherTag, AssetName);
-
+            string downloadLink = client.GetAssetDownloadURL(GithubLauncherTag, AssetName());
+            
             WebClient webClient = new WebClient();
+            ProgressWindow window = new();
+
+            window.BindProgressWindow(webClient);
             await webClient.DownloadFileTaskAsync(downloadLink, GetFullPath(LauncherExecutablePath));
+            
             File.Create(GetFullPath(LauncherVersionPath)).Close();
             await File.WriteAllTextAsync(GetFullPath(LauncherVersionPath), versionID);
         }
@@ -107,7 +122,7 @@ public class DeadLauncherBoot
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
                 Process process = new Process();
-                process.StartInfo.FileName = GetFullPath(LauncherExecutablePath+".exe");
+                process.StartInfo.FileName = GetFullPath(LauncherExecutablePath);
                 process.Start();
             }
             else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
@@ -123,6 +138,38 @@ public class DeadLauncherBoot
                 launch.Start();
             }
             Process.GetCurrentProcess().Close();    
+        }
+    }
+    private class ProgressWindow
+    {
+        public string State = "Updating Launcher";
+        private int progress;
+
+        public void BindProgressWindow(WebClient webClient)
+        {
+            ThreadPool.QueueUserWorkItem(delegate { CreateWindow(); });
+            webClient.DownloadProgressChanged += TrackProgress;
+        }
+
+        private void TrackProgress(object sender, DownloadProgressChangedEventArgs e)
+        {
+            progress = e.ProgressPercentage;
+        }
+
+        public void CreateWindow()
+        {
+            Raylib.InitWindow(300, 100, "Deadays Updater");
+            while (!Raylib.WindowShouldClose())
+            {
+                int width = (int)((progress / 100f) * 260);
+                Raylib.ClearBackground(Color.White);
+                Raylib.BeginDrawing();
+                {
+                    Raylib.DrawText(State + $"({progress})", 22, 10, 21, Color.Blue);
+                    Raylib.DrawRectangle(20, 50, width, 40, Color.Blue);   
+                }
+                Raylib.EndDrawing();
+            }
         }
     }
 }
